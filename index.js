7c0a5ae746392884cdbcf1b66bbf5ddd779a31b4
Top Dreamer <128351913+topmsdreamer@users.noreply.github.com>
2023-10-24 06:49:45 -0500

    Before GSAP animation

--- index.js
+++ index.js
@@ -69,16 +69,24 @@ class Enemy {
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 30, 'blue')
+const player = new Player(x, y, 10, 'white')
 const projectiles = []
 const enemies = []
 
 function spawnEnemies() {
     setInterval(() => {
-        const x = Math.random() * canvas.width
-        const y = Math.random() * canvas.height
-        const radius = 30
-        const color = 'green'
+        const radius = Math.random() * (30 - 4) + 4
+        let x, y
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height
+        } else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }
+
+
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`
 
         const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
 
@@ -93,12 +101,54 @@ function spawnEnemies() {
 
 spawnEnemies()
 
+let animationId
 function animate() {
-    requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    animationId = requestAnimationFrame(animate)
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
-    projectiles.forEach(projectile => projectile.update())
-    enemies.forEach(enemy => enemy.update())
+    projectiles.forEach((projectile, index) => {
+        projectile.update()
+
+        if (projectile.x + projectile.radius < 0 ||
+            projectile.x - projectile.radius > canvas.width ||
+            projectile.y + projectile.radius < 0 ||
+            projectile.y - projectile.radius > canvas.height
+        ) {
+            setTimeout(() => {
+                projectiles.splice(index, 1);
+            })
+        }
+    })
+    enemies.forEach((enemy, index) => {
+        enemy.update()
+
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+
+        // end game
+        if (dist - enemy.radius - player.radius < 1) {
+            cancelAnimationFrame(animationId)
+        }
+
+        projectiles.forEach((projectile, projectileIndex) => {
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+
+            // object touch
+            if (dist - enemy.radius - projectile.radius < 1) {
+                if (enemy.radius - 10 > 10) {
+                    enemy.radius -= 10
+                    setTimeout(() => {
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0)
+                } else {
+                    setTimeout(() => {
+                        enemies.splice(index, 1)
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0)
+                }
+            }
+        })
+    })
 }
 
 window.addEventListener('click', (event) => {
@@ -109,15 +159,15 @@ window.addEventListener('click', (event) => {
         )
 
         const velocity = {
-            x: Math.cos(angle),
-            y: Math.sin(angle)
+            x: Math.cos(angle) * 4,
+            y: Math.sin(angle) * 4
         }
 
         const projectile = new Projectile(
             player.x,
             player.y,
             5,
-            'red',
+            'white',
             velocity
         )
 
